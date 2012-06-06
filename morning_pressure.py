54f4abeae43a05d88e8d1dbf2909ed3267dbded8
Kenneth Nielsen <k.nielsen81@gmail.com>
2012-06-06 15:54:58 +0200

    Font size polishing of morning graph

--- morning_pressure.py
+++ morning_pressure.py
@@ -5,6 +5,7 @@
 import MySQLdb
 import numpy as np
 from datetime import timedelta, datetime
+import matplotlib
 import matplotlib.pyplot as plt
 
 try:
@@ -20,6 +21,7 @@ CURSOR = DB.cursor()
 FROM_DATE = '2012-03-08 00:30'
 TO_DATE = '2012-04-21 2:00'
 TABLE = 'pressure_microreactorNG'
+#TABLE = 'pressure_stm312'
 
 CURSOR.execute("select unix_timestamp(date(time)), avg(pressure) from "
                "{table} where hour(time) = 1 and minute(time) "
@@ -37,20 +39,26 @@ TZ = timedelta(hours=1)
 XTICK_POSITIONS = []
 DATETIMES = []
 for n, timestamp in enumerate(reversed(DATA[:, 0])):
-    if n % REDUCTION == 0:
+    if n % REDUCTION == REDUCTION - 1:
         XTICK_POSITIONS.append(timestamp + 3600)
         DATETIMES.append(datetime.fromtimestamp(timestamp) + TZ)
-XLABELS = [date_time.strftime('%b-%d %H:%M') for date_time in DATETIMES]
+#XLABELS = [date_time.strftime('%b-%d %H:%M') for date_time in DATETIMES]
+XLABELS = [date_time.strftime('%b-%d') for date_time in DATETIMES]
+
+# Define figure font sizes
+matplotlib.rc('xtick', labelsize=14)
+matplotlib.rc('ytick', labelsize=14)
+YLABELSIZE = 22
 
 # Make the figure
 FIG, AX = plt.subplots(1, 1, 1)
-AX.bar(DATA[:, 0] + 3600, DATA[:, 1], width=12000, color='black',
+AX.bar(DATA[:, 0] + 3600, DATA[:, 1], width=16000, color='black',
        align='center', log=True)
-AX.set_ylabel('Pressure / torr')
+AX.set_ylabel('Pressure / torr', size=YLABELSIZE)
 
 # Exchange the xticks
 AX.set_xticks(XTICK_POSITIONS)
 AX.set_xticklabels(XLABELS, rotation=30, horizontalalignment='right')
-plt.subplots_adjust(bottom=0.2)
+#plt.subplots_adjust(bottom=0.2)
 
 plt.show()
